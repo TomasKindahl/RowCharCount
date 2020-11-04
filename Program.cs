42cff18548373e83130a733c37071e924642460f
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-04 10:54:13 +0100

    found wordcount code at URL https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,21 @@ namespace RowCharCount
             Console.Write("Ange ett filnamn: ");
             string fileName = Console.ReadLine();
             Console.WriteLine("Öppnar: {0}", fileName);
+            // Från: https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-a-text-file-in-C-Sharp/
+            // Read file using StreamReader. Reads file line by line
+            using (StreamReader file = new StreamReader(fileName))
+            {
+                int counter = 0;
+                string ln;
+
+                while ((ln = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(ln);
+                    counter++;
+                }
+                file.Close();
+                Console.WriteLine($"File has {counter} lines.");
+            }
         }
     }
 }
