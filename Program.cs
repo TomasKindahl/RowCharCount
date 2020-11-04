6d987c20049a07d93508e7a9bc2579ef356f20ad
Tomas Kindahl <tomas.kindahl@molk.se>
2020-11-04 10:45:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RowCharCount
+{
+    // FAS 1. Uppgift 3 - Rad/tecken-räknare
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ange ett filnamn: ");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Öppnar: {0}", fileName);
+        }
+    }
+}
